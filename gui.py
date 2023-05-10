ceed1ec3ddec2e35e835b3e6e5bed05a940a965a
m20re <marvinorellana@unomaha.edu>
2023-05-10 16:43:23 -0500

    Added equation frame and button, and a generator func.

--- gui.py
+++ gui.py
@@ -14,13 +14,41 @@ class GUI:
         self.label_title = Label(self.frame_title, font=("Arial",25), text="Math Checker")
         self.label_title.pack()
         self.frame_title.pack(pady=10)
-        
-         # label num_2 & num_2 frame
+
+         # label num_1 & num_2 frame
         self.frame_num_display = Frame(self.window)
         self.label_num_1 = Label(self.frame_num_display, text=' First Number', font=("Arial", 8))
         self.label_num_2 = Label(self.frame_num_display, text=' Second Number', font=("Arial", 8))
 
         self.frame_num_display.pack(pady=5, anchor='w')
+
+        # equation frame
+        self.number1 = StringVar()
+        self.number2 = StringVar()
+        self.operation = StringVar()
+    
+        self.frame_equation = Frame(self.window)
+        self.label_num1 = Label(self.frame_equation, font=("Arial", 25), textvariable=self.number1)
+        self.label_operation = Label(self.frame_equation, font=("Arial", 25), textvariable=self.operation)
+        self.label_num2 = Label(self.frame_equation, font=("Arial", 25), textvariable=self.number2)
+        self.entry_answer = Entry(self.frame_equation, font=("Arial", 25), width=5)
+
+        self.label_num1.pack_forget()
+        self.label_operation.pack_forget()
+        self.label_num2.pack_forget()
+        self.entry_answer.pack_forget()
+        self.entry_answer = Entry(self.frame_equation, font=("Arial", 25), width=5)
+        
+        self.frame_equation.pack(pady=5)
+
+        # new equation button
+        self.frame_generate_button = Frame(self.window)
+        self.generate_button = Button(self.frame_generate_button, text='Generate Equation', command=self.generate_equation_and_result)
+        self.generate_button.pack(padx=5)
+        self.frame_generate_button.pack(anchor='s',pady=10) 
+
+    def generate_equation_and_result(self) -> list:
+        return
 
 
 
